a46810906c9b980dd5d0f7353e084a0da6dc3f81
Huckjay <huckjaywai@gmail.com>
2012-07-15 00:22:53 +0100

    Lucky Lotto basic page

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sandbox::Application.config.secret_token = '9a65400df782857d6d91eaa41bbdb8a44379a3b352f92a09812d4b3f3c8510fe4f6c4130c3a301c63fb28f5d98166907bba54328774b71d13b6b29b50de4b704'

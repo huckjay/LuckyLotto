1e05a28ab49c9e74c78e20e4cad713344c06314d
Huckjay <huckjaywai@gmail.com>
2012-08-14 00:11:08 +0100

    Made basic User model inc secure passwords

--- Gemfile
+++ Gemfile
@@ -1,11 +1,16 @@
 source 'http://rubygems.org'
 
 gem 'rails', '3.2.6'
+gem 'bootstrap-sass', '2.0.0'
+gem 'bcrypt-ruby', '3.0.1'
 
-group :development do
+group :development, :test do
   gem 'sqlite3', '1.3.5'
 	gem 'rspec-rails', '2.10.0'
+	gem 'guard-rspec', '0.5.5'
 end
+
+gem 'annotate', '2.5.0'
 
 
 # Gems used only for assets and not required
@@ -16,10 +21,15 @@ group :assets do
   gem 'uglifier', '1.2.3'
 end
 
-gem 'jquery-rails', '2.0.0'
+	gem 'jquery-rails', '2.0.0'
 
 group :test do
 	gem 'capybara', '1.1.2'
+	gem 'rb-inotify', '0.8.8'
+	gem 'libnotify', '0.5.9'
+	gem 'guard-spork', '0.3.2'
+	gem 'spork', '0.9.0'
+
 end
 
 group :production do
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,50 +2,81 @@ require 'spec_helper'
 
 describe "Static Pages" do
 
-#Checks using have_content - quite general
+#Checks using have_content - quite general, just picks up any text it finds in the html
 
 	describe "Help page" do
 		it "should have the content 'Help'" do
-			visit '/static_pages/help'
+			visit help_path
 			page.should have_content('Help')
 		end
 	end
 
+	describe "Contact page" do
+		it "should have the content 'Contact'" do
+			visit contact_path	# 	'static_pages/contact'
+			page.should have_content('Contact')
+		end
+	end
+
+	describe "About page" do
+		it "should have the content 'About'" do
+			visit about_path
+			page.should have_content('About')
+		end
+	end
+
 	describe "Home page" do
 		it "should have the content 'Help'" do
-			visit '/static_pages/home'
+#			visit '/static_pages/home'
+			visit root_path
 			page.should have_content('Home')
 		end
 	end
 
-#Checks using have_selector - more specific
+#Checks using have_selector - more specific, will check the h1 tag for specific text
 
 	describe "Home page" do
+		before { visit root_path }
 
 		it "should have the h1 'Home'" do
-			visit '/static_pages/home'
-			page.should have_selector('h1', :text =>"HomePage")
+#			visit root_path	-	no longer needed as we have used the before so all methods in this block will visit root_path
+			page.should have_selector('h1', :text =>"Luck \"E\" Lottery page")
 		end
 
-		it "should have a title 'Home'" do	
-			visit '/static_pages/home'
-			page.should have_selector('title', :text => "Home")
+		it "should have a title 'Home'" do
+			page.should have_selector('title', :text => "Lucky Lotto App")
+		end
+
+# WHAT IS THIS???
+		it "should not have a custom page title" do
+			page.should_not have_selector('title', :text => '| Home')
 		end
 
 	end
 
 	describe "Help page" do
+		before { visit help_path }
 
 		it "should have the h1 'Help'" do
-			visit '/static_pages/help'
 			page.should have_selector('h1', :text =>"Help")
 		end
 
-		it "should have a title 'Help'" do	
-			visit '/static_pages/help'
+		it "should have a title 'Help'" do
 			page.should have_selector('title', :text => "Help")
 		end
 
+	end
+
+	it "should have the right links on the layout" do
+		visit root_path
+		click_link "Sign Up"
+		page.should have_selector 'title', text: full_title('Sign up')
+		click_link "About"
+		page.should have_selector 'title', text: full_title('About')
+		click_link "Contact"
+		page.should have_selector 'title', text: full_title('Contact')
+
+	
 	end
 
 end
